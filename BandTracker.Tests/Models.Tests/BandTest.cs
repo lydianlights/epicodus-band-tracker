aa689197e365e80d1df2eb9ca538b0ba4b9a64a3
LydianLights <rane.fields@gmail.com>
2017-11-03 11:05:41 -0700

    Add CRUD functionality for bands

--- BandTracker.Tests/Models.Tests/BandTest.cs
+++ BandTracker.Tests/Models.Tests/BandTest.cs
@@ -6,12 +6,90 @@ using BandTracker.Models;
 namespace BandTracker.Models.Tests
 {
     [TestClass]
-    public class BandTest
+    public class BandTest : IDisposable
     {
-        // [TestMethod]
-        // public void Method_Description_ExpectedValue()
-        // {
-        //
-        // }
+        private Band sampleBand_Transatlantic = new Band("Transatlantic");
+        private Band sampleBand_Transatlantic_2 = new Band("Transatlantic");
+        private Band sampleBand_Haken = new Band("Haken");
+
+        public BandTest()
+        {
+            DBConfiguration.ConnectionString = "server=localhost; user id=root; password=root; port=8889; database=band_tracker_test;";
+        }
+        public void Dispose()
+        {
+            Band.ClearAll();
+        }
+
+        [TestMethod]
+        public void Equals_TwoEqualObjects_True()
+        {
+            Assert.AreEqual(sampleBand_Transatlantic, sampleBand_Transatlantic_2);
+        }
+        [TestMethod]
+        public void Equals_TwoUnequalObjects_False()
+        {
+            Assert.AreNotEqual(sampleBand_Transatlantic, sampleBand_Haken);
+        }
+        [TestMethod]
+        public void GetAll_DatabaseIsEmptyAtFirst_EmptyList()
+        {
+            int result = Band.GetAll().Count;
+
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void Save_SavesToDatabase_EntrySaved()
+        {
+            sampleBand_Transatlantic.Save();
+            Band result = Band.GetAll()[0];
+
+            Assert.AreEqual(sampleBand_Transatlantic, result);
+        }
+        [TestMethod]
+        public void GetAll_GetsAllEntries_AllEntries()
+        {
+            sampleBand_Transatlantic.Save();
+            sampleBand_Haken.Save();
+
+            List<Band> result = Band.GetAll();
+            List<Band> test = new List<Band> {sampleBand_Transatlantic, sampleBand_Haken};
+
+            CollectionAssert.AreEqual(test, result);
+        }
+        [TestMethod]
+        public void FindById_GetEntryWithMatchingId_Entry()
+        {
+            sampleBand_Transatlantic.Save();
+            sampleBand_Haken.Save();
+
+            Band result = Band.FindById(sampleBand_Haken.Id);
+
+            Assert.AreEqual(sampleBand_Haken, result);
+        }
+        [TestMethod]
+        public void UpdateAtId_UpdateEntryWithMatchingId_EntryUpdated()
+        {
+            sampleBand_Transatlantic.Save();
+            string newName = "Not the place";
+            Band.UpdateAtId(sampleBand_Transatlantic.Id, newName);
+
+            Band result = Band.FindById(sampleBand_Transatlantic.Id);
+            Band test = new Band(newName, sampleBand_Haken.Id);
+
+            Assert.AreEqual(test, result);
+        }
+        [TestMethod]
+        public void DeleteAtId_DeletesEntryAtMatchingId_EntryDeleted()
+        {
+            sampleBand_Transatlantic.Save();
+            sampleBand_Haken.Save();
+            Band.DeleteAtId(sampleBand_Haken.Id);
+
+            List<Band> result = Band.GetAll();
+            List<Band> test = new List<Band> {sampleBand_Transatlantic};
+
+            CollectionAssert.AreEqual(test, result);
+        }
     }
 }
