b8cdad0e41e00f1bdfda50c72ef5d56f6493614b
LydianLights <rane.fields@gmail.com>
2017-11-03 08:35:34 -0700

    Add initial files from template

---
+++ BandTracker.Tests/Models.Tests/_MyClassTest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BandTracker.Models;
+
+namespace BandTracker.Models.Tests
+{
+  [TestClass]
+  public class _MyClassTest
+  {
+    [TestMethod]
+    public void Method_Description_ExpectedValue()
+    {
+
+    }
+  }
+}
---
+++ BandTracker/Models/Database.cs
@@ -0,0 +1,18 @@
+using System;
+using MySql.Data.MySqlClient;
+using BandTracker;
+
+namespace BandTracker.Models
+{
+  public class DB
+  {
+    public static MySqlConnection Connection
+    {
+        get
+        {
+            MySqlConnection conn = new MySqlConnection(DBConfiguration.ConnectionString);
+            return conn;
+        }
+    }
+  }
+}
