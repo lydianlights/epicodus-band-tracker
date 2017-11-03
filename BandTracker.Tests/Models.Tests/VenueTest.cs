8a897bd88081ed258d26d3b99ee2d920c4487f45
LydianLights <rane.fields@gmail.com>
2017-11-03 10:57:43 -0700

    Add CRUD functionality to venue

--- BandTracker.Tests/Models.Tests/VenueTest.cs
+++ BandTracker.Tests/Models.Tests/VenueTest.cs
@@ -6,11 +6,20 @@ using BandTracker.Models;
 namespace BandTracker.Models.Tests
 {
     [TestClass]
-    public class VenueTest
+    public class VenueTest : IDisposable
     {
         private Venue sampleVenue_ThePlace = new Venue("The Place");
         private Venue sampleVenue_ThePlace_2 = new Venue("The Place");
         private Venue sampleVenue_ASpot = new Venue("A Spot");
+
+        public VenueTest()
+        {
+            DBConfiguration.ConnectionString = "server=localhost; user id=root; password=root; port=8889; database=band_tracker_test;";
+        }
+        public void Dispose()
+        {
+            Venue.ClearAll();
+        }
 
         [TestMethod]
         public void Equals_TwoEqualObjects_True()
@@ -21,6 +30,66 @@ namespace BandTracker.Models.Tests
         public void Equals_TwoUnequalObjects_False()
         {
             Assert.AreNotEqual(sampleVenue_ThePlace, sampleVenue_ASpot);
+        }
+        [TestMethod]
+        public void GetAll_DatabaseIsEmptyAtFirst_EmptyList()
+        {
+            int result = Venue.GetAll().Count;
+
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void Save_SavesToDatabase_EntrySaved()
+        {
+            sampleVenue_ThePlace.Save();
+            Venue result = Venue.GetAll()[0];
+
+            Assert.AreEqual(sampleVenue_ThePlace, result);
+        }
+        [TestMethod]
+        public void GetAll_GetsAllEntries_AllEntries()
+        {
+            sampleVenue_ThePlace.Save();
+            sampleVenue_ASpot.Save();
+
+            List<Venue> result = Venue.GetAll();
+            List<Venue> test = new List<Venue> {sampleVenue_ThePlace, sampleVenue_ASpot};
+
+            CollectionAssert.AreEqual(test, result);
+        }
+        [TestMethod]
+        public void FindById_GetEntryWithMatchingId_Entry()
+        {
+            sampleVenue_ThePlace.Save();
+            sampleVenue_ASpot.Save();
+
+            Venue result = Venue.FindById(sampleVenue_ASpot.Id);
+
+            Assert.AreEqual(sampleVenue_ASpot, result);
+        }
+        [TestMethod]
+        public void UpdateAtId_UpdateEntryWithMatchingId_EntryUpdated()
+        {
+            sampleVenue_ThePlace.Save();
+            string newName = "Not the place";
+            Venue.UpdateAtId(sampleVenue_ThePlace.Id, newName);
+
+            Venue result = Venue.FindById(sampleVenue_ThePlace.Id);
+            Venue test = new Venue(newName, sampleVenue_ASpot.Id);
+
+            Assert.AreEqual(test, result);
+        }
+        [TestMethod]
+        public void DeleteAtId_DeletesEntryAtMatchingId_EntryDeleted()
+        {
+            sampleVenue_ThePlace.Save();
+            sampleVenue_ASpot.Save();
+            Venue.DeleteAtId(sampleVenue_ASpot.Id);
+
+            List<Venue> result = Venue.GetAll();
+            List<Venue> test = new List<Venue> {sampleVenue_ThePlace};
+
+            CollectionAssert.AreEqual(test, result);
         }
     }
 }
--- BandTracker/Models/Venue.cs
+++ BandTracker/Models/Venue.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace BandTracker.Models
 {
@@ -28,6 +29,118 @@ namespace BandTracker.Models
       public override int GetHashCode()
       {
           return this.Name.GetHashCode();
+      }
+      public void Save()
+      {
+          MySqlConnection conn = DB.Connection;
+          conn.Open();
+
+          var cmd = conn.CreateCommand() as MySqlCommand;
+          cmd.CommandText = @"INSERT INTO venues (name) VALUES (@Name);";
+          cmd.Parameters.Add(new MySqlParameter("@Name", this.Name));
+          cmd.ExecuteNonQuery();
+          this.Id = (int)cmd.LastInsertedId;
+
+          conn.Close();
+          if (conn != null)
+          {
+              conn.Dispose();
+          }
+      }
+      public static List<Venue> GetAll()
+      {
+          var output = new List<Venue> {};
+          MySqlConnection conn = DB.Connection;
+          conn.Open();
+
+          var cmd = conn.CreateCommand() as MySqlCommand;
+          cmd.CommandText = @"SELECT * FROM venues;";
+          var rdr = cmd.ExecuteReader() as MySqlDataReader;
+          while(rdr.Read())
+          {
+              int id = rdr.GetInt32(0);
+              string name = rdr.GetString(1);
+              output.Add(new Venue(name, id));
+          }
+          conn.Close();
+          if (conn != null)
+          {
+              conn.Dispose();
+          }
+          return output;
+      }
+      public static Venue FindById(int targetId)
+      {
+          MySqlConnection conn = DB.Connection;
+          conn.Open();
+
+          var cmd = conn.CreateCommand() as MySqlCommand;
+          cmd.CommandText = @"SELECT * FROM venues WHERE id = @id;";
+          cmd.Parameters.Add(new MySqlParameter("@id", targetId));
+          var rdr = cmd.ExecuteReader() as MySqlDataReader;
+          int id = 0;
+          string name = "";
+          while(rdr.Read())
+          {
+              id = rdr.GetInt32(0);
+              name = rdr.GetString(1);
+          }
+          var output = new Venue(name, id);
+
+          conn.Close();
+          if (conn != null)
+          {
+              conn.Dispose();
+          }
+          return output;
+      }
+      public static void UpdateAtId(int targetId, string newName)
+      {
+          MySqlConnection conn = DB.Connection;
+          conn.Open();
+
+          var cmd = conn.CreateCommand() as MySqlCommand;
+          cmd.CommandText = @"UPDATE venues SET name = @name WHERE id = @id;";
+          cmd.Parameters.Add(new MySqlParameter("@name", newName));
+          cmd.Parameters.Add(new MySqlParameter("@id", targetId));
+          cmd.ExecuteNonQuery();
+
+          conn.Close();
+          if (conn != null)
+          {
+              conn.Dispose();
+          }
+      }
+      public static void DeleteAtId(int targetId)
+      {
+          MySqlConnection conn = DB.Connection;
+          conn.Open();
+
+          var cmd = conn.CreateCommand() as MySqlCommand;
+          cmd.CommandText = @"DELETE FROM concerts WHERE venue_id = @id; DELETE FROM venues WHERE id = @id;";
+          cmd.Parameters.Add(new MySqlParameter("@id", targetId));
+          cmd.ExecuteNonQuery();
+
+          conn.Close();
+          if (conn != null)
+          {
+              conn.Dispose();
+          }
+      }
+      public static void ClearAll()
+      {
+          MySqlConnection conn = DB.Connection;
+          conn.Open();
+
+          var cmd = conn.CreateCommand() as MySqlCommand;
+          cmd.CommandText = @"DELETE FROM concerts; DELETE FROM venues;";
+          cmd.ExecuteNonQuery();
+
+          conn.Close();
+          if (conn != null)
+          {
+              conn.Dispose();
+          }
       }
   }
 }
